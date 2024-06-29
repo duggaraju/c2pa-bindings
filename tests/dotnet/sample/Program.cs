d34692549442df4c67ba0aa8494e73fe5022b95a
Isaiah Carrington <t-isaiah@microsoft.com>
2024-06-29 16:14:43 -0700

    Added some sample code

--- tests/dotnet/sample/Program.cs
+++ tests/dotnet/sample/Program.cs
@@ -1,188 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
-using System.Text.Json;
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
+
 using Azure.Core;
 using Azure.Identity;
-using Azure.Security.KeyVault.Keys;
-using Azure.Security.KeyVault.Keys.Cryptography;
-using Azure.Security.KeyVault.Secrets;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.CodeSigning;
+using Azure.CodeSigning.Models;
+
 using C2pa;
+using C2paExceptions;
 
-internal class Program
-{
-    public static void Main(string inputFile, string? outputFile = null)
+namespace C2paSample{
+    class Program
     {
-        Console.WriteLine("Version: {0}", Sdk.Version);
-        Console.WriteLine("Supprted Extensions: {0}", string.Join(",", Sdk.SupportedExtensions));
-
-        if (string.IsNullOrEmpty(inputFile))
-            throw new ArgumentNullException(nameof(inputFile), "No filename was provided.");
-        if (!File.Exists(inputFile))
-            throw new IOException($"No file exists with the filename of {inputFile}.");
-
-        if (outputFile == null)
-            ValidateFile(inputFile);
-        else
-            SignFile(inputFile, outputFile);
-    }
-
-    private static void SignFile(string inputFile, string outputFile)
-    {
-        string extension = Path.GetExtension(inputFile).Substring(1);
-        //var signer = new OpenSslSigner();
-        var signer = new KeyVaultSigners(new DefaultAzureCredential(true));
-
-        var settings = new ManifestBuilderSettings
+        static void Main(string[] args)
         {
-            ClaimGenerator = "C# Binding test"
-        };
-
-        Manifest manifest = new ()
-        {
-            Title = "C# Test Image",
-            Format = "jpg",
-            ClaimGeneratorInfo = [new("C# Test", "1.0.0")],
-            Assertions = [ new("stds.schema-org.CreativeWork", new AssertionData("http://schema.org", "CreativeWork", [new AuthorInfo("Person", "Isaiah Carrington")]))]
-        };
-
-        var manifestDefinition = manifest.GetManifestJson();
-
-        manifestDefinition = $$"""
-{
-    "claim_generator_info": [
-        {
-            "name": "C# test",
-            "version": "0.0.1"
+            Console.WriteLine("Hello World!");
         }
-    ],
-    "format": "{{extension}}",
-    "title": "C# Test Image",
-    "thumbnail": {
-        "format": "jpg",
-        "identifier": "1234"
-    },
-    "ingredients": [],
-    "assertions": [
-        {   "label": "stds.schema-org.CreativeWork",
-            "data": {
-                "@context": "http://schema.org/",
-                "@type": "CreativeWork",
-                "author": [
-                    {   "@type": "Person",
-                        "name": "Prakash Duggaraju"
-                    }
-                ]
-            },
-            "kind": "Json"
-        }
-    ]
-}
-""";
 
-        Console.WriteLine("Signing manifest");
-        var builder = new ManifestBuilder(settings, signer.Config, signer, manifestDefinition);
-        builder.Sign(inputFile, outputFile);
-        Console.WriteLine("Signing successful");
-    }
-
-    private static void ValidateFile(string filename)
-    {
-        Console.WriteLine($"Reading manifest from file: {filename}");
-
-        var reader = new ManifestStoreReader();
-        var store = reader.ReadFromFile(filename);
-        if (store != null)
+        class TrustedSigningCallback : ISignerCallback
         {
-            var activeManifest = store.Manifests[store.ActiveManifest];
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            Console.WriteLine("Manifest: {0}", JsonSerializer.Serialize(store, options));
-        }
-        else
-        {
-            Console.WriteLine("No Manifest found: {0}", filename);
-        }
-    }
-}
+            private readonly CertificateProfileClient _client;
 
-class KeyVaultSigners : ISignerCallback
-{
-    const string KeyVaultUri = "https://kv-8c538cfad6204d9cb88a.vault.azure.net/";
-    const string SecretName = "media-provenance-pem";
+            const string EndpointUri = "https://eus.codesigning.azure.net/";
+            static readonly Azure.CodeSigning.Models.SignatureAlgorithm Algorithm = Azure.CodeSigning.Models.SignatureAlgorithm.PS384;
+            const string CertificateProfile = "media-provenance-sign";
+            const string AccountName = "ts-80221a56b4b24529a43e";
 
-    const string KeyName = "media-provenance-sign";
-
-    private readonly TokenCredential _credential;
-
-    public KeyVaultSigners(TokenCredential credential)
-    {
-        _credential = credential;
-    }
-
-    public string GetCertificates()
-    {
-        var client = new SecretClient(new Uri(KeyVaultUri), _credential);
-        KeyVaultSecret secret = client.GetSecretAsync(SecretName).Result;
-        return secret.Value!;
-    }
-
-    public int Sign(ReadOnlySpan<byte> data, Span<byte> hash)
-    {
-        var client = new KeyClient(new Uri(KeyVaultUri), _credential);
-        KeyVaultKey key = client.GetKey(KeyName);
-        var crypto = new CryptographyClient(new Uri(key.Key.Id), _credential);
-        var result = crypto.SignData(SignatureAlgorithm.RS384, data.ToArray());
-        result.Signature.CopyTo(hash);
-        return result.Signature.Length;
-    }
-
-    public SignerConfig Config => new SignerConfig
-    {
-        Alg = "ps384",
-        Certs = GetCertificates(),
-        TimeAuthorityUrl = "http://timestamp.digicert.com",
-        UseOcsp = false
-    };
-}
-
-
-class OpenSslSigner : ISignerCallback
-{
-    const string KeyFile = "/home/krishndu/rust/c2pa-rs/sdk/tests/fixtures/certs/ps256.pem";
-    const string CertFile = "/home/krishndu/rust/c2pa-rs/sdk/tests/fixtures/certs/ps256.pub";
-
-
-    public SignerConfig Config => new SignerConfig
-    {
-        Alg = "ps256",
-        Certs = File.ReadAllText(CertFile),
-        TimeAuthorityUrl = "http://timestamp.digicert.com",
-        UseOcsp = false
-    };
-
-    public int Sign(ReadOnlySpan<byte> data, Span<byte> hash)
-    {
-        Console.WriteLine("Signing data of length: {0}", data.Length);
-        var path = Path.GetTempPath();
-        var dataFile = Path.Combine(path, Path.GetTempFileName());
-        var outFile = Path.Combine(path, Path.GetTempFileName());
-        File.WriteAllBytes(dataFile, data.ToArray());
-        var process = new Process
-        {
-            StartInfo = new ProcessStartInfo
+            public TrustedSigningCallback()
             {
-                FileName = "openssl",
-                Arguments = $"dgst -sign {KeyFile} -sha256 -out {outFile} {dataFile}",
-                RedirectStandardOutput = false,
-                RedirectStandardError = false,
-                UseShellExecute = false,
-                CreateNoWindow = true
+                string endpoint = "https://eus.codesigning.azure.net/";
+                _client = new (new DefaultAzureCredential(), new Uri(endpoint), null);
             }
-        };
-        process.Start();
-        process.WaitForExit();
-        Console.WriteLine("Process exit code: {0}", process.ExitCode);
-        var bytes = File.ReadAllBytes(outFile);
-        bytes.CopyTo(hash);
-        return bytes.Length;
+
+            public int Sign(ReadOnlySpan<byte> data, Span<byte> hash)
+            {
+
+                byte[] digest = SHA384.HashData(data.ToArray());
+
+                SignRequest req = new(Algorithm, digest);
+                CertificateProfileSignOperation operation = _client.StartSign(AccountName, CertificateProfile, req);
+                SignStatus status = operation.WaitForCompletion();
+                status.Signature.CopyTo(hash);
+
+                return status.Signature.Length;
+            }
+
+            public string GetCertificates() {
+                Random random = new();
+                byte[] hash = new byte[32];
+                random.NextBytes(hash);
+                byte[] digest = SHA384.HashData(hash);
+                
+                SignRequest request = new (Algorithm, digest);
+                CertificateProfileSignOperation operation = _client.StartSign(AccountName, CertificateProfile, request);
+                SignStatus status = operation.WaitForCompletion();
+                var cmsData = new SignedCms();
+
+                return "";
+            }
+
+            public SignerConfig Config => new()
+            {
+                Alg = "ps384",
+                Certs = "This is a sample certificate.",
+                TimeAuthorityUrl = "http://timestamp.digicert.com",
+                UseOcsp = false
+            };
+        }
     }
 }
